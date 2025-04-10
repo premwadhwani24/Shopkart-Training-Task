7f05985b6561335d538431d05bbfd31ff5a44eac
dbc2201 <divyanshb22@gmail.com>
2025-04-10 19:01:09 +0530

    initial commit

---
+++ src/test/java/io/github/dbc2201/spring/boot/demos/shopkart/ShopkartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dbc2201.spring.boot.demos.shopkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
