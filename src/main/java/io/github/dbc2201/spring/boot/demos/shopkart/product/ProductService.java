1ebddc44ea7450d6576736071892e192e78c390b
dbc2201 <divyanshb22@gmail.com>
2025-04-10 19:04:00 +0530

    add a Product LinkedList to the ProductService class

--- src/main/java/io/github/dbc2201/spring/boot/demos/shopkart/product/ProductService.java
+++ src/main/java/io/github/dbc2201/spring/boot/demos/shopkart/product/ProductService.java
@@ -2,6 +2,14 @@ package io.github.dbc2201.spring.boot.demos.shopkart.product;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductService {
+	private List<Product> productList;
+
+	public ProductService() {
+		productList = new ArrayList<>();
+	}
 }
